dd1dd4810bbd3229ade3d385484ff97c30b9c0f1
Kauê Lima <kauelima@Kaues-MacBook-Air.local>
2023-08-26 16:32:18 -0300

    Primeiro EndPoint - Responsavel

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ResponsavelModule } from './responsavel/responsavel.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
+  imports: [ResponsavelModule],
 })
 export class AppModule {}
---
+++ src/responsavel/responsavel.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { ResponsavelService } from './responsavel.service';
+import { ResponsavelController } from './responsavel.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Module({
+  imports: [PrismaModule],
+  controllers: [ResponsavelController],
+  providers: [ResponsavelService,PrismaService],
+})
+export class ResponsavelModule {}
