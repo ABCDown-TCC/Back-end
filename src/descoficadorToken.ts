e115637369a8ac2a7179540d6c69e06caca2858c
KAUE SILVA DE LIMA <22282191@dsn127m1246730.cfp127aluno.local>
2023-10-02 11:30:27 -0300

    detalhes

--- src/descoficadorToken.ts
+++ src/descoficadorToken.ts
@@ -3,28 +3,30 @@ import * as jwt from 'jsonwebtoken';
 
 interface User {
   id: string;
+  email: string;
 }
 
+@Injectable()
 @Injectable()
 export class Decodificadora {
   private readonly secretKey = process.env.SECRET_KEY;
 
   async decodificadorToken(token: string): Promise<string | null> {
     try {
-      const [bearer, codigo] = token.split(' ');
+      const [bearer, codigo] = token.split('');
 
       if (bearer !== 'Bearer' || !codigo) {
         throw new Error('Token inválido');
       }
 
-      const decoded = jwt.verify(codigo, this.secretKey) as User;
+      const decoded = (await jwt.verify(codigo, this.secretKey)) as User;
+
       const { id } = decoded;
       return id;
     } catch (error) {
-      // Aqui você pode lidar com erros de decodificação ou token inválido
-      console.error('Erro ao decodificar o token:', error);
+      // Trate o erro e forneça uma mensagem de erro significativa
+      console.error('Erro ao decodificar o token:', error.message);
       return null;
     }
   }
 }
-
